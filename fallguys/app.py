7224bb8e386c5229476009c9da0d13282ea02d02
Khokasai <khokasai.jin@gmail.com>
2020-12-09 14:35:02 +0900

    test_heroku

--- fallguys/app.py
+++ fallguys/app.py
@@ -4,7 +4,7 @@ import random
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from PIL import Image
 from pathlib import Path
 
